5fa77a457d33ce75040c631de282408b451164d9
Simeon Raychev <simeon0022@gmail.com>
2023-01-21 14:17:39 +0200

    React DOM and component basics

--- Plain-React/src/script.js
+++ Plain-React/src/script.js
@@ -1,5 +1,10 @@
 let rootElement = document.getElementById("root");
 
-let reactElement = React.createElement("h1", {}, "Hello React!");
+let reactElement = React.createElement(
+  "header",
+  { class: "site-header" },
+  React.createElement("h1", { id: "main-heading" }, "Hello React"),
+  React.createElement("h2", {}, "The best framework in the world!")
+);
 
 ReactDOM.render(reactElement, rootElement);
